98d2ceca5feb6f28adb73f9269a19c580efa4275
Nesmeian <lenya.kozlovskiy.97@mail.ru>
2024-12-28 17:33:15 +0300

    feat:after game start,game delete from rooms

--- src/controlers/response/createGame.ts
+++ src/controlers/response/createGame.ts
@@ -2,10 +2,17 @@ import { CustomWebSocket } from 'src/interfacess';
 import searchRoomPlayersNames from '..//../dataBase/db/controlers/searchRoomUsers';
 import terminalMessage from '../../utils/consoleLogMessageCollor';
 import sendWsToChoseConnectsions from '..//..//..//src/utils/sendWsToChoseConnects';
+import { data } from '..//..//dataBase/db';
+import updateRoom from './updateRoom';
 export default function createGame(
   ws: CustomWebSocket,
   indexRoom: string,
 ): void {
+  const { rooms } = data;
+  const gameRoomIndex = rooms.findIndex(({ roomId }) => roomId === indexRoom);
+  rooms.splice(gameRoomIndex, 1);
+  console.log('jack', rooms);
+  updateRoom(ws);
   const usersNames = searchRoomPlayersNames(indexRoom);
   const usersIds = usersNames.map(({ id }) => id);
   const createGameReg = {
